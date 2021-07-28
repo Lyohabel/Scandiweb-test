b041ca6607d40cca1c8cef44343650134b8106c6
Lev <lyoha.bel2020@gmail.com>
2021-07-28 10:30:46 +0300

    product updated

--- src/Components/Categories/Product/Product.js
+++ src/Components/Categories/Product/Product.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { client, Query} from "@tilework/opus";
 import OverallData from '../../../Context';
-//import imgProd1 from '../../../ImagesTemp/black.png';
 import * as styles from './Product.module.css'
+
+const COLOR = 'color'
+const COLOR_C = 'Color'
 
 class Product extends React.Component {
   constructor(props) {
@@ -17,29 +19,32 @@ class Product extends React.Component {
       prices: '',
       instok: '',
       attributes: '',
-      description: '',      
+      description: '',
+
       attr_1: '',
       attr_2: '',
       attr_3: '',
+
+      attr_1Id: '',
+      attr_2Id: '',
+      attr_3Id: '',
+
       sizeButton: {
         a : styles.size,
         b : styles.sizeActive
       },
+
       colorButton: {
         a : styles.color,
         b : styles.colorActive
       },
+
       productAdded: 'no',
       add: styles.add     
     }
-
-    //this.methodeName = this.methodeName.bind(this)
-    //this.props.changeCountCart = this.props.changeCountCart.bind(this)
-	
   }
 
   componentDidMount() {
-    //console.log(this.state.productAdded)
     const id = window.location.href.split('/')[4];
 
     client.setEndpoint("http://localhost:4000/graphql");
@@ -56,14 +61,21 @@ class Product extends React.Component {
       const name = result.product.name
       const instock = result.product.inStock
       const img = gallery[0]
+
       const prices = [
         result.product.prices[0].amount,
         result.product.prices[1].amount,
         result.product.prices[2].amount,
         result.product.prices[3].amount,
         result.product.prices[4].amount
-      ]      
+      ]
+
       const attributes = result.product.attributes
+
+      const attr_1Id = result.product.attributes[0] ? result.product.attributes[0].id : ''
+      const attr_2Id = result.product.attributes[1] ? result.product.attributes[1].id : ''        
+      const attr_3Id = result.product.attributes[2] ? result.product.attributes[2].id : ''
+
       const attr_1 = result.product.attributes[0] ? result.product.attributes[0].items : ''
       const attr_2 = result.product.attributes[1] ? result.product.attributes[1].items : ''
       const attr_3 = result.product.attributes[2] ? result.product.attributes[2].items : ''
@@ -80,20 +92,23 @@ class Product extends React.Component {
       instock: instock,
       prices: prices,
       attributes: attributes,
+
+      attr_1Id: attr_1Id,
+      attr_2Id: attr_2Id,
+      attr_3Id: attr_3Id,
+
       attr_1: attr_1,
       attr_2: attr_2,
       attr_3: attr_3,
+
       description: description    
-      });
-      //this.props.setDefaultAttributes()
-      //console.log(this.props.def)                
+      });              
      });
      
   }
-//******************************************************************** */
+
   componentDidUpdate() {
     if (this.state.productAdded !== 'no') {
-      //console.log(this.state.productAdded)     
 
       const id = window.location.href.split('/')[4];
 
@@ -111,6 +126,7 @@ class Product extends React.Component {
         const name = result.product.name
         const instock = result.product.inStock
         const img = gallery[0]
+
         const prices = [
           result.product.prices[0].amount,
           result.product.prices[1].amount,
@@ -119,6 +135,11 @@ class Product extends React.Component {
           result.product.prices[4].amount
         ]      
         const attributes = result.product.attributes
+
+        const attr_1Id = result.product.attributes[0] ? result.product.attributes[0].id : ''
+        const attr_2Id = result.product.attributes[1] ? result.product.attributes[1].id : ''
+        const attr_3Id = result.product.attributes[2] ? result.product.attributes[2].id : ''
+
         const attr_1 = result.product.attributes[0] ? result.product.attributes[0].items : ''
         const attr_2 = result.product.attributes[1] ? result.product.attributes[1].items : ''
         const attr_3 = result.product.attributes[2] ? result.product.attributes[2].items : ''
@@ -135,13 +156,18 @@ class Product extends React.Component {
         instock: instock,
         prices: prices,
         attributes: attributes,
+
+        attr_1Id: attr_1Id,
+        attr_2Id: attr_2Id,
+        attr_3Id: attr_3Id,
+
         attr_1: attr_1,
         attr_2: attr_2,
         attr_3: attr_3,
+
         description: description,
         productAdded: 'no'    
-        });
-        //console.log(this.state.productAdded)                                
+        });                                       
       });
     }
   }
@@ -151,8 +177,7 @@ class Product extends React.Component {
       ...this.state,     
       productAdded: 'yes'    
       });
-  }
-  //*************************************************************************** */
+  } 
 
   addAttrToCart(id, attr, value) {
     if (!window.localStorage.getItem('cart')) return;
@@ -164,9 +189,7 @@ class Product extends React.Component {
       if (element.name === id) {
         element[attr] = +value;             
       }      
-    })
-    //console.log(attr)
-    //console.log(jsonCart);
+    })   
 
     window.localStorage.setItem('cart', JSON.stringify(jsonCart));
   }
@@ -185,13 +208,11 @@ class Product extends React.Component {
     const attributeName = this.findAttributeName(event);    
        
     buttons.forEach(element => {
-      element.classList.remove((attributeName !== 'color') ? this.state.sizeButton.b : this.state.colorButton.b)
-      element.classList.add((attributeName !== 'color') ? this.state.sizeButton.a : this.state.colorButton.a)
-     //if (attributeName !== 'color')
+      element.classList.remove((attributeName !== COLOR) ? this.state.sizeButton.b : this.state.colorButton.b)
+      element.classList.add((attributeName !== COLOR) ? this.state.sizeButton.a : this.state.colorButton.a)
       element.setAttribute('choosed', "no");
     });
-    currentButton.classList.add((attributeName !== 'color') ? this.state.sizeButton.b : this.state.colorButton.b)
-    //if (attributeName !== 'color')
+    currentButton.classList.add((attributeName !== COLOR) ? this.state.sizeButton.b : this.state.colorButton.b)    
     currentButton.setAttribute('choosed', "yes");     
   }
 
@@ -211,114 +232,74 @@ class Product extends React.Component {
       else return `${this.state.attributes[ind].id.toUpperCase()}:`        
   }
 
-  createButtons(attrs, attrName) {
+  createButtons(attrs, btnStyle) {
     return attrs && attrs.map((item, index, array) =>
       <button id={index} key={item.value} value={item.value} choosed={(index === 0) ? "yes" : "no"}
       onClick={(event) => {this.markAttribute(event); this.addAttrToCart(this.state.id, this.findAttributeName(event), event.target.id); this.props.changeAttributes(event)}}          
-      className={(attrName !== 'COLOR') ? ((index === 0) ? this.state.sizeButton.b : this.state.sizeButton.a) : ((index === 0) ? this.state.colorButton.b : this.state.colorButton.a)}
-      style={attrName !== 'COLOR' ? {width: `calc(95% / ${array.length})`} : {backgroundColor: item.value, width: `calc(95% / ${array.length})`, color: (item.displayValue === 'Black' || item.displayValue === 'Blue') ? '#fff' : '#000'}}>
-        {item.value}
-        <span>{item.displayValue}</span>
+      className={(btnStyle !== COLOR_C) ? ((index === 0) ? this.state.sizeButton.b : this.state.sizeButton.a) : ((index === 0) ? this.state.colorButton.b : this.state.colorButton.a)}
+      style={btnStyle !== COLOR_C ? {width: `calc(95% / ${array.length})`} : {backgroundColor: item.value, width: `calc(95% / ${array.length})`, color: (item.displayValue === 'Black' || item.displayValue === 'Blue') ? '#fff' : '#1D1F22'}}>
+        {btnStyle !== COLOR_C ? item.value : ''}
+        <span className={styles.displayValue}>{item.displayValue}</span>
       </button>
       )
-  }  
-
-  //  style={{width: `calc(95% / ${array.length})`}}>{item.displayValue}</button>
-  // style={{backgroundColor: item.value, width: `calc(95% / ${array.length})`, color: (item.displayValue === 'Black' || item.displayValue === 'Blue') ? //'#fff' : '#000'}}>{item.displayValue}</button> 
-
-  createSizesButtons(attrs) {
-    //console.log(attrs)
-    return attrs && attrs.map((item, index, array) =>
-      <button id={index} key={item.value} value={item.value} onClick={(event) => {this.markAttribute(event); this.addAttrToCart(this.state.id, this.findAttributeName(event), event.target.id); this.props.changeAttributes(event)}} className={(index === 0) ? this.state.sizeButton.b : this.state.sizeButton.a} choosed={(index === 0) ? "yes" : "no"}  style={{width: `calc(95% / ${array.length})`}}>{item.displayValue}</button>
-      )
   }
 
-  createColorsButtons(attrs) {
-    return attrs && attrs.map((item, index, array) =>
-      <button id={index} key={item.value} value={item.value} onClick={(event) => {this.markAttribute(event); this.addAttrToCart(this.state.id, this.findAttributeName(event), event.target.id); this.props.changeAttributes(event)}} className={(index === 0) ? this.state.colorButton.b : this.state.colorButton.a} style={{backgroundColor: item.value, width: `calc(95% / ${array.length})`, color: (item.displayValue === 'Black' || item.displayValue === 'Blue') ? '#fff' : '#000'}}>{item.displayValue}</button>
-      )
-  }
-  
-  createCapacityButtons(attrs) {
-    return attrs && attrs.map((item, index, array) =>
-      <button id={index} key={item.value} value={item.value} onClick={(event) => {this.markAttribute(event); this.addAttrToCart(this.state.id, this.findAttributeName(event), event.target.id); this.props.changeAttributes(event)}} className={(index === 0) ? this.state.sizeButton.b : this.state.sizeButton.a} style={{width: `calc(95% / ${array.length})`}}>{item.displayValue}</button>
-      )
-  }
-
-  createOptionButtons(attrs) {
-    return attrs && attrs.map((item, index, array) =>
-      <button id={index} key={item.value} value={item.value} onClick={(event) => {this.markAttribute(event); this.addAttrToCart(this.state.id, this.findAttributeName(event), event.target.id); this.props.changeAttributes(event)}} className={(index === 0) ? this.state.sizeButton.b : this.state.sizeButton.a} style={{width: `calc(95% / ${array.length})`}}>{item.displayValue}</button>
-      )
-  }
-
-  setAttributes() {
-    if (this.state.attributes.length === 0) return ''
-      else if (this.state.attributes[0].id === 'Size') {
+  setAttributes(order) {
+    switch(order) {  // eslint-disable-line
+      case 1:  
+      if (this.state.attributes.length === 0) return ''
+      else if (this.state.attributes[0].id !== COLOR_C) {
          return (
           <div className={styles.chooseSize}>
-            {this.createSizesButtons(this.state.attr_1)}           
+            {this.createButtons(this.state.attr_1, '')}           
           </div> 
-          )} else if (this.state.attributes[0].id === 'Color') {
+          )} else {
             return (
               <div className={styles.chooseSize}>
-                {this.createColorsButtons(this.state.attr_1)}           
+                {this.createButtons(this.state.attr_1, COLOR_C)}           
               </div> 
-            )} else if (this.state.attributes[0].id === 'Capacity') {
-              return (
-                <div className={styles.chooseSize}>
-                  {this.createCapacityButtons(this.state.attr_1)}           
-                </div> 
-              )} 
-  }
-
-  setAttributes_2() {
-    if (this.state.attributes.length < 2) return ''
-      else if (this.state.attributes[1].id === 'Size') {
+            )}
+    
+      case 2:  
+      if (this.state.attributes.length < 2) return ''
+      else if (this.state.attributes[1].id !== COLOR_C) {
          return (
           <div className={styles.chooseSize}>
-            {this.createSizesButtons(this.state.attr_2)}           
+            {this.createButtons(this.state.attr_2, '')}           
           </div> 
-          )} else if (this.state.attributes[1].id === 'Color') {
+          )} else {
             return (
               <div className={styles.chooseSize}>
-                {this.createColorsButtons(this.state.attr_2)}           
+                {this.createButtons(this.state.attr_2, COLOR_C)}           
               </div> 
-            )} else if (this.state.attributes[1].id === 'Capacity') {
-              return (
-                <div className={styles.chooseSize}>
-                  {this.createCapacityButtons(this.state.attr_2)}           
-                </div> 
-              )} else { return (
+            )}
+
+        case 3:
+          if (this.state.attributes.length < 3) return ''
+          else if (this.state.attributes[2].id !== COLOR_C) {
+             return (
+              <div className={styles.chooseSize}>
+                {this.createButtons(this.state.attr_3, '')}           
+              </div> 
+              )} else {
+                return (
                   <div className={styles.chooseSize}>
-                    {this.createOptionButtons(this.state.attr_2)}           
+                    {this.createButtons(this.state.attr_3, COLOR_C)}           
                   </div> 
-                )} 
+                )}        
+    }    
   }
 
-  setAttributes_3() {
-    if (this.state.attributes.length < 3) return ''
-      else if (this.state.attributes[1].id === 'Size') {
-         return (
-          <div className={styles.chooseSize}>
-            {this.createSizesButtons(this.state.attr_3)}           
-          </div> 
-          )} else if (this.state.attributes[1].id === 'Color') {
-            return (
-              <div className={styles.chooseSize}>
-                {this.createColorsButtons(this.state.attr_3)}           
-              </div> 
-            )} else if (this.state.attributes[1].id === 'Capacity') {
-              return (
-                <div className={styles.chooseSize}>
-                  {this.createCapacityButtons(this.state.attr_3)}           
-                </div> 
-              )} else { return (
-                  <div className={styles.chooseSize}>
-                    {this.createOptionButtons(this.state.attr_3)}           
-                  </div> 
-                )} 
-  }  
+  returnAttributes(arr) {
+    return arr && arr.map((item, index) =>
+      <div key={item.id} className="attrWrapper">
+        <h4 className={styles.sizeTitle}>{this.state.attributes[index] ? this.showAttributeName(index) : ''}</h4>
+        {this.setAttributes(index+1)}
+      </div>
+    )    
+  }
 
+ 
   componentWillUnmount() {
     this.props.setDefaultAttributes()
   }  
@@ -338,20 +319,7 @@ class Product extends React.Component {
                 <h3 className={styles.title}>{this.state.brand}</h3>
                 <span className={styles.subtitle}>{this.state.name}</span>
 
-                <div className="attrWrapper">
-                <h4 className={styles.sizeTitle}>{this.showAttributeName(0)}</h4>
-                {this.setAttributes()}
-                </div>
-
-                <div className="attrWrapper">
-                  <h4 className={styles.sizeTitle}>{this.state.attributes[1] ? this.showAttributeName(1) : ''}</h4>
-                  {this.setAttributes_2()}
-                </div>
-
-                <div className="attrWrapper">
-                <h4 className={styles.sizeTitle}>{this.state.attributes[2] ? this.showAttributeName(2) : ''}</h4>
-                {this.setAttributes_3()}                  
-                </div>
+                {this.state.attributes ? this.returnAttributes(this.state.attributes) : ''}                
 
                 <h4 className={styles.priceTitle}>Price:</h4>
 
