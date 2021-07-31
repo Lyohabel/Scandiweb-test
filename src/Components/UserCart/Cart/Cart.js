31b1d5724e7ffa6d408520e5a3f75d9350031272
maria <dairymolko@gmail.com>
2021-07-31 11:36:29 +0300

    add changes

--- src/Components/UserCart/Cart/Cart.js
+++ src/Components/UserCart/Cart/Cart.js
@@ -7,36 +7,8 @@ import * as styles from './Cart.module.css'
 class Cart extends React.Component {
   constructor(props) { // eslint-disable-line
     super(props);
-    this.state = {
-      jsonCart: '',
-      productsNumber: '',           
-      cartData: [],
-      id: '',     
-      gallery: '',
-      galleryLength: '',
-      img: '',
-      brand: '',
-      name: '',
-      prices: '',
-
-      price_0: '',
-      price_1: '',
-      price_2: '',
-      price_3: '',
-      price_4: '',
-
-      instok: '',
-      attributes: '',
-      description: '',
-
-      attr_1Id: '',
-      attr_2Id: '',
-      attr_3Id: '',
-
-      attr_1: '',
-      attr_2: '',
-      attr_3: '',
-
+    this.state = {  
+      currentProducts: [],
       sizeButton: {
         a : styles.size,
         b : styles.sizeActive
@@ -53,18 +25,8 @@ class Cart extends React.Component {
       galleryId: ''
     }
     
-    this.createCartList = this.createCartList.bind(this)
-    this.selectCurrency = this.selectCurrency.bind(this)
     this.creatGallery = this.creatGallery.bind(this)
     this.showAnoterImage = this.showAnotherImage.bind(this)
-  }  
-
-  selectCurrency(index) {
-    if (this.context.currencyNumber === 0) return this.state.price_0[index];
-      else if (this.context.currencyNumber === 1) return this.state.price_1[index];
-        else if (this.context.currencyNumber === 2) return this.state.price_2[index];
-          else if (this.context.currencyNumber === 3) return this.state.price_3[index];
-            else if (this.context.currencyNumber === 4) return this.state.price_4[index];
   }
 
   creatGalleryList(index) {    
@@ -134,65 +96,6 @@ class Cart extends React.Component {
     
     )
   }
-    
-  createCartList() {        
-    return this.state.jsonCart && this.state.jsonCart.map((item, index) =>
-      <li className={styles.cartItem} key={index}>
-         <span className={styles.cartLine}></span>
-
-         <div className={styles.cartItemWrapper}>
-          <div className={styles.prodInf}>
-            <h4 className={styles.cartItemTitle}>{this.state.brand[index]}</h4>
-            <span className={styles.cartItemSubtitle}>{this.state.name[index]}</span>
-
-            <div className={styles.prodPrice}><span className={styles.currencySimbol}>{this.context.currencySimbol}</span><span className={styles.currencyAmount}>{this.selectCurrency(index)}</span>
-            </div>
-
-            <div className={styles.attributesWrapper}>
-              {/* <div className={styles.attributeTypeWrapper}>
-                <button className={styles.attrBut}>S</button>
-                <button className={styles.attrBut}>M</button>
-              </div> */}
-
-              {/* {this.creatChoosedAttributes()} */}
-
-              <div className={styles.attributeTypeWrapper}>
-                <button className={styles.attrBut}>S</button>
-                <button className={styles.attrBut}>M</button>
-              </div>
-
-              <div className={styles.attributeTypeWrapper}>
-                <button className={styles.attrBut}>S</button>
-                <button className={styles.attrBut}>M</button>
-              </div>             
-            </div>          
-
-            {/* <div className={styles.colorButtons}>
-              <button className={styles.sBut}>S</button>
-              <button className={styles.mBut}>M</button>
-            </div>             */}
-          </div>
-
-          <div className={styles.prodImage}>
-            <div className={styles.countButtons}>
-              <button className={styles.plusBut}>&#43;</button>
-                <span>1</span>                    
-              <button className={styles.minusBut}>&#8722;</button>
-            </div>
-
-            <div className={styles.galleryWrapper}>
-
-              {this.creatGalleryList(index)}
-
-              <button onClick={() => this.showAnotherImage('prev', item.id)} className={(this.state.galleryLength[index] > 1) ? this.state.sliderDisplayLeft : this.state.imgHidden}></button>
-              
-              <button onClick={() => this.showAnotherImage('next', item.id)} className={(this.state.galleryLength[index] > 1) ? this.state.sliderDisplayRight : this.state.imgHidden}></button>
-            </div>
-          </div>
-         </div>
-      </li>
-    )
-  }
   
   componentDidMount() {
     if (!window.localStorage.getItem('cart')) return;
@@ -201,192 +104,42 @@ class Cart extends React.Component {
     let jsonCart = JSON.parse(cart);
     //jsonCart.splice(0,1);
 
-    this.setState({
-      ...this.state,        
-      jsonCart: jsonCart,
-      productsNumber: jsonCart.length 
-    })
+    // this.setState({
+    //   ...this.state,      
+    //   currentProducts: [],  
+    //   jsonCart: jsonCart,
+    //   productsNumber: jsonCart.length 
+    // })
 
     let queryArgs = []
     jsonCart.forEach(element => {
       queryArgs.push(element.name)
     })
+
     
     client.setEndpoint("http://localhost:4000/graphql");
 
-    let cartId = [],
-        cartGallery = [],
-        cartGalleryLength = [],
-        cartBrand = [],
-        cartName = [],
-        cartInstock = [],
-        cartImg = [],
-        cartPrices = [],
-        cartAttributes = [],
-
-        cartAttr_1Id = [],
-        cartAttr_2Id = [],
-        cartAttr_3Id = [],
-
-        cartAttr_1 = [],
-        cartAttr_2 = [],
-        cartAttr_3 = [],
-
-        cartDescription = []        
-
-        queryArgs.forEach((element, index) => {
-      let queryName = `${element}`
+     queryArgs.forEach((element, index) => {
+      let queryName = `${element}`;
       queryName = new Query("product", true)
       .addArgument("id", "String!", element)   
-      .addFieldList(["id", "name", "inStock", "gallery", "description", "brand", "attributes {id, name, type, items {displayValue, value, id}}", "prices {currency,amount }"])
+      .addFieldList(["id", "name", "inStock", "gallery", "description", "brand", "prices { amount }, attributes {id, name, type, items {displayValue, value, id}}"])
 
       client.post(queryName).then(result => {
-        const id = result.product.id
-        const gallery = result.product.gallery
-        const galleryLength = result.product.gallery.length
-        const brand = result.product.brand
-        const name = result.product.name
-        const instock = result.product.inStock
-        const img = gallery[0]
-        const prices = [
-          result.product.prices[0],
-          result.product.prices[1],
-          result.product.prices[2],
-          result.product.prices[3],
-          result.product.prices[4]
-        ]      
-        const attributes = result.product.attributes
-
-        const attr_1Id = result.product.attributes[0] ? result.product.attributes[0].id : ''
-        const attr_2Id = result.product.attributes[1] ? result.product.attributes[1].id : ''
-        const attr_3Id = result.product.attributes[2] ? result.product.attributes[2].id : ''
-
-        const attr_1 = result.product.attributes[0] ? result.product.attributes[0].items : ''
-        const attr_2 = result.product.attributes[1] ? result.product.attributes[1].items : ''
-        const attr_3 = result.product.attributes[2] ? result.product.attributes[2].items : ''
-
-        const description = result.product.description
-
-        cartId.push(id)
-        cartGallery.push(gallery)
-        cartGalleryLength.push(galleryLength)
-        cartBrand.push(brand)
-        cartName.push(name)
-        cartInstock.push(instock)
-        cartImg.push(img)
-        cartPrices.push(prices)
-        cartAttributes.push(attributes)
-
-        cartAttr_1Id.push(attr_1Id)
-        cartAttr_2Id.push(attr_2Id)
-        cartAttr_3Id.push(attr_3Id)
-
-        cartAttr_1.push(attr_1)
-        cartAttr_2.push(attr_2)
-        cartAttr_3.push(attr_3)
-
-        cartDescription.push(description)
-
-
-        // const itemData = result.product.name
-        // newCartData.push(itemData)        
-
-        this.setState({
-          ...this.state,          
-          id: cartId,
-          gallery: cartGallery,
-          galleryLength: cartGalleryLength,
-          img: cartImg,
-          brand: cartBrand,
-          name: cartName,
-          instock: cartInstock,
-          prices: cartPrices,
-          attributes: Array.from(cartAttributes),
-
-          attr_1Id: cartAttr_1Id,
-          attr_2Id: cartAttr_2Id,
-          attr_3Id: cartAttr_3Id,
-
-          attr_1: cartAttr_1,
-          attr_2: cartAttr_2,
-          attr_3: cartAttr_3,
-
-          description: cartDescription    
-          });
-        //console.log(typeof itemData)
-        //console.log(this.state.prices)
-
-        let newPrice_0 = []
-        let newPrice_1 = []
-        let newPrice_2 = []
-        let newPrice_3 = []
-        let newPrice_4 = []
-    
-        this.state.prices.map(item => { // eslint-disable-line
-          let price0 = JSON.stringify(item[0]).split(':')[2]
-          const price_0 = price0.slice(0, price0.length - 1);
-
-          let price1 = JSON.stringify(item[1]).split(':')[2]
-          const price_1 = price1.slice(0, price1.length - 1);
-
-          let price2 = JSON.stringify(item[2]).split(':')[2]
-          const price_2 = price2.slice(0, price2.length - 1);
-
-          let price3 = JSON.stringify(item[3]).split(':')[2]
-          const price_3 = price3.slice(0, price3.length - 1);
-
-          let price4 = JSON.stringify(item[4]).split(':')[2]
-          const price_4 = price4.slice(0, price4.length - 1);
-          
-          
-          newPrice_0.push(price_0)
-          newPrice_1.push(price_1)
-          newPrice_2.push(price_2)
-          newPrice_3.push(price_3)
-          newPrice_4.push(price_4)
-    
-          this.setState({
-            ...this.state,
-            price_0: newPrice_0,
-            price_1: newPrice_1,
-            price_2: newPrice_2,
-            price_3: newPrice_3,
-            price_4: newPrice_4
-          })      
-        })
-
-        // **********************************************************************************************************************************
-        let newAttr_1_1 = []
-        let newAttr_1_2 = []
-        let newAttr_1_3 = []       
-    
-        this.state.attr_1[0].map(item => { // eslint-disable-line
-          let x = JSON.stringify(item).split('"value":"')[1]
-          let y = JSON.stringify(x).split('"')[1]
-          const nA0 = y.slice(0, y.length - 1)
-          newAttr_1_1.push(nA0) 
-        })
-
-        if (this.state.attr_1[1]) this.state.attr_1[1].map(item => { // eslint-disable-line
-          let x = JSON.stringify(item).split('"value":"')[1]
-          let y = JSON.stringify(x).split('"')[1]
-          const nA1 = y.slice(0, y.length - 1)
-          newAttr_1_2.push(nA1) 
-        })
-
-        //console.log(JSON.stringify(this.state.attr_1[0]))
-        console.log(this.state.attr_1)
-        console.log(newAttr_1_1)
-        console.log(newAttr_1_2)
-        //console.log(newAttr_1_2)
-        // console.log(this.state.attributes)
-        // console.log(this.state.attr_1)
-        // console.log(this.state.jsonCart)
-      })
-    })
-  }
+        result = JSON.parse(JSON.stringify(result.product))
+        result.prices = result.prices.map(item => item.amount)
+        this.setState((prev) => ({
+          ...prev, currentProducts: [...prev.currentProducts, result]
+      }))
+      }
+       
+      )
+  })
+}
 
   render() {
+
+  console.log(this.state.currentProducts)
     return (
       <section className={styles.cart}>                                     
         <div className='container'>
@@ -395,8 +148,69 @@ class Cart extends React.Component {
             {/* <button onClick={() => console.log(this.state.cartData[0].name)}>XXXXX</button> */}
             <ul className={styles.productList}>
 
-              {this.createCartList()}
-              
+              {this.state.currentProducts && this.state.currentProducts.length && this.state.currentProducts.map((item, index) => {
+                console.log(item)
+                return (
+                  <li className={styles.cartItem} key={index}>
+                  <span className={styles.cartLine}></span>
+         
+                  <div className={styles.cartItemWrapper}>
+                   <div className={styles.prodInf}>
+                     <h4 className={styles.cartItemTitle}>{item.brand}</h4>
+                     <span className={styles.cartItemSubtitle}>{item.name}</span>
+         
+                     <div className={styles.prodPrice}><span className={styles.currencySimbol}>{this.context.currencySimbol}</span>
+                     <span className={styles.currencyAmount}>{item.prices[this.context.currencyNumber]}</span>
+                     </div>
+         
+                     <div className={styles.attributesWrapper}>
+                       {item.attributes && item.attributes.map(item => item.items.map(attr => <>{attr.displayValue} {attr.name}</>))}
+                       {/* <div className={styles.attributeTypeWrapper}>
+                         <button className={styles.attrBut}>S</button>
+                         <button className={styles.attrBut}>M</button>
+                       </div> */}
+         
+                       {/* {this.creatChoosedAttributes()} */}
+         
+                       <div className={styles.attributeTypeWrapper}>
+                         <button className={styles.attrBut}>S</button>
+                         <button className={styles.attrBut}>M</button>
+                       </div>
+         
+                       <div className={styles.attributeTypeWrapper}>
+                         <button className={styles.attrBut}>S</button>
+                         <button className={styles.attrBut}>M</button>
+                       </div>             
+                     </div>          
+         
+                     {/* <div className={styles.colorButtons}>
+                       <button className={styles.sBut}>S</button>
+                       <button className={styles.mBut}>M</button>
+                     </div>             */}
+                   </div>
+         
+                   <div className={styles.prodImage}>
+                     <div className={styles.countButtons}>
+                       <button className={styles.plusBut}>&#43;</button>
+                         <span>1</span>                    
+                       <button className={styles.minusBut}>&#8722;</button>
+                     </div>
+         
+                     <div className={styles.galleryWrapper}>
+         
+                       {/* {this.creatGalleryList(index)} */}
+         
+                       {/* <button onClick={() => this.showAnotherImage('prev', item.id)} className={(this.state.galleryLength[index] > 1) ? this.state.sliderDisplayLeft : this.state.imgHidden}></button> */}
+                       
+                       {/* <button onClick={() => this.showAnotherImage('next', item.id)} className={(this.state.galleryLength[index] > 1) ? this.state.sliderDisplayRight : this.state.imgHidden}></button> */}
+                     </div>
+                   </div>
+                  </div>
+               </li>
+             
+                )
+              }
+    )}
             </ul>            
           </div>
         </div>
